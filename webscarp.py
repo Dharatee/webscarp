89ac1d6068c3245a64989aeffbf4c5deb9017dd9
Angrybird <shrestha_dss@hotmail.com>
2019-05-20 00:05:41 +0545

    print div and classes

--- webscarp.py
+++ webscarp.py
@@ -13,11 +13,13 @@ uClient.close()
 #html parsing
 page_soup=soup(page_html,"html.parser")
 
-print(page_soup.body)
-print(page_soup.head)
-print(page_soup.title)
+#print(page_soup.body)
+#print(page_soup.head)
+#print(page_soup.title) 
+#or
+#print(page_soup.head.title)
 
-"""#header of the page
+#header of the page or the title
 page_soup.h1
 
 
@@ -27,11 +29,19 @@ page_soup.p
 #converting into csv, traverse DOM elements of the HTML
 page_soup.body.span
 
-containers=page_soup.findAll("div",{"class":"item-container"})
-len(containers)
+#use find method to access parts instead of aforementioned print(page_soup.body)
 
-containers[0]  #should see HTML for this index"""
+#gives the div ID of section 1, the first one it finds
+containers=page_soup.find("div """",{"class":"item-container"}""" )
 
 
+#get more than one
+#find_all() or findAll()
+containers=page_soup.findAll('div')[2]  #div with ID of section 2
+
+containers=page_soup.find(id='section-1')
+containers=page_soup.find(class_='item-container') #class is a reserved word so use class_ .
+
+print(containers)
 
 
