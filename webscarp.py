1cfa42a430dcf99ff34e586a18cfb787017628c6
Angrybird <shrestha_dss@hotmail.com>
2019-05-19 23:34:17 +0545

    print parts of web page

--- webscarp.py
+++ webscarp.py
@@ -8,7 +8,30 @@ uClient=uReq(my_url)
 page_html=uClient.read()
 uClient.close()
 
+
+
 #html parsing
 page_soup=soup(page_html,"html.parser")
+
+print(page_soup.body)
+print(page_soup.head)
+print(page_soup.title)
+
+"""#header of the page
+page_soup.h1
+
+
+#not found on the page, could be a hidden tagline
+page_soup.p
+
+#converting into csv, traverse DOM elements of the HTML
+page_soup.body.span
+
+containers=page_soup.findAll("div",{"class":"item-container"})
+len(containers)
+
+containers[0]  #should see HTML for this index"""
+
+
 
 
